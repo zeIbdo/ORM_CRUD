6907c502479a72823905a9bfcd4ef2937c74c923
zeIbdo <ibrahimrm@code.edu.az>
2024-08-07 17:26:40 +0400

    Add project files.

---
+++ ORM_CRUD/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ORM_CRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_CRUD
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-6KFQ39N;Database=ProductsCA;Trusted_Connection=true");
+            base.OnConfiguring(optionsBuilder);
+        }
+        DbSet<Product> Products { get; set; }
+        DbSet<Category> Categories { get; set; }
+    }
+}
